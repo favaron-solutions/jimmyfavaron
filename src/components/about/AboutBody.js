47c68fbe661284aed4ffec4f0a24696dd785ca55
Jimmy Favaron <jimmyfavaron@gmail.com>
2018-04-04 21:31:54 -0600

    fixed state

--- src/components/about/AboutBody.js
+++ src/components/about/AboutBody.js
@@ -4,14 +4,6 @@ import Blurbs from './blurbs/Blurbs.js';
 import { Container, Row, Col } from 'reactstrap';
 import Chart from '../chart/Chart.js';
 
-var dataArray=[
-    {x: 'NodeJS', y: 90, color: '#555'}
-    {x: 'ReactJS', y: 70, color: '#555'}
-    {x: 'PHP', y: 85, color: '#555'}
-    {x: 'Cake', y: 80, color: '#555'}
-];
-
-
 export default class AboutBody extends React.Component {
   render() {
     return (
@@ -19,10 +11,10 @@ export default class AboutBody extends React.Component {
         <Blurbs/>
         <Divider/>
         <Row>
-          <Col sm={12} md={6} className="d-flex justify-content-center" chartInfo={dataArray}>
+          <Col sm={12} md={6} className="d-flex justify-content-center" >
             <Chart/>
           </Col>
-          <Col sm={12} md={6} className="d-flex justify-content-center" chartInfo={dataArray}>
+          <Col sm={12} md={6} className="d-flex justify-content-center" >
             <Chart/>
           </Col>
         </Row>
