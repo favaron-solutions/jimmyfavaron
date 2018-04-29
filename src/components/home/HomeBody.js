2576245732a0149ed143d5de65492afbb7b2a370
Jimmy Favaron <jimmyfavaron@gmail.com>
2018-04-29 01:42:39 -0600

    adding featured project

--- src/components/home/HomeBody.js
+++ src/components/home/HomeBody.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Divider from '../divider/Divider.js';
-import { Container } from 'reactstrap';
+import { Container, Button, Jumbotron } from 'reactstrap';
 import PageTitle from '../header/page-title/PageTitle.js';
 import Welcome from './welcome/Welcome.js';
 
@@ -11,6 +11,16 @@ export default class HomeBody extends React.Component {
         <PageTitle pageTitle={this.props.pageTitle}/>
         <Divider/>
         <Welcome/>
+          <Jumbotron>
+            <h1 className="display-3">Scratch</h1>
+            <h4>Featured Project</h4>
+            <p className="lead"> </p>
+            <hr className="my-2" />
+            <p>Simple note taking app using the serverless framework to host a static ReactJS App with a serverless NodeJS backend. Scratch is completely responsive and will work on any device. Create an account and take some notes yourself!</p>
+            <p className="lead">
+              <a href="http://d32ugifdwq37wt.cloudfront.net/"><Button color="primary"> Try it out! </Button></a>
+            </p>
+          </Jumbotron>
         <Divider/>
       </Container>
     );
