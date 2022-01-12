79018ac11b2b979b39338f41c7478ce914346be9
jimmy <jimmyfavaron@gmail.com>
2022-01-12 00:33:54 -0700

    adding cert-validations 3

--- acm.tf
+++ acm.tf
@@ -15,7 +15,11 @@ resource "aws_acm_certificate" "ssl_certificate" {
 # Uncomment the validation_record_fqdns line if you do DNS validation instead of Email.
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert-validations : record.fqdn]
+  validation_record_fqdns = aws_route53_record.cert-validations.*.fqdn
+
+  timeouts {
+    create = "120m"
+  }
 }
 
 # resource "aws_acm_certificate_validation" "cert-validation" {
