408ae0f450f5eb89cec54cb0a8ca82dccaf52f64
jimmy <jimmyfavaron@gmail.com>
2022-01-13 23:07:45 -0700

    round 17

--- s3.tf
+++ s3.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "www_bucket" {
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
   acl = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+  policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
 
   website {
     redirect_all_requests_to = "https://www.${var.domain_name}"
