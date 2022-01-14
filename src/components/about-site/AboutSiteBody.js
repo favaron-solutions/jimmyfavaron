2b1fb1715757af2f099e707d0e402446210c6bfe
jimmy <jimmyfavaron@gmail.com>
2022-01-14 09:16:04 -0700

    fixing svg

--- src/components/about-site/AboutSiteBody.js
+++ src/components/about-site/AboutSiteBody.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Divider from '../divider/Divider.js';
 import { Container } from 'reactstrap';
 import PageTitle from '../header/page-title/PageTitle.js';
-import cicd from '../../assets/ci-cd.drawio.svg';
+import cicd from '../../assets/ci-cd.svg';
 
 export default class AboutSiteBody extends React.Component {
   render() {
@@ -11,9 +11,8 @@ export default class AboutSiteBody extends React.Component {
         <PageTitle pageTitle={this.props.pageTitle}/>
         <Divider/>
         <div className="d-flex justify-content-center">
-        <p>heyo</p>
-        
-        <img src={cicd} alt=""/>
+        <p>heyo</p>       
+        <img id="cicd-pipeline" src={cicd} alt=""/>
         <p>testing</p>
         </div>
         <Divider/>
